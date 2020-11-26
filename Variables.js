3dbfbdf464c7d190edc28309daf5b233d8dbb2fb
Swechchha Ranjit <swechchha.ranjit@verscend.com>
2020-11-26 09:19:50 +0545

    learned about datatypes

---
+++ Variables.js
@@ -0,0 +1,22 @@
+//Primitive
+var a = "swechchha";
+var b = true;
+var c = 345.6;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; // not a number
+var x = "dsdf"/34;
+//console.log(x);
+
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MAX_VALUE);
+var d = 3n ** 123n;
+
+var e = undefined;
+var f = null;
+
+//Structural
+var g = ["a", "b", "c"];
+var h = { name: "swechchha", age: 12 };
+
+console.log(typeof a);
